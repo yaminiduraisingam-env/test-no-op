77167bb36016dc716e4775852f415f56ee7a3a17
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-08 11:38:48 -0400

    Update main.tf

--- env-b/main.tf
+++ env-b/main.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.0"
 }
 
-variable "received_value" {
-  description = "Value passed in from Environment A"
-  type        = string
-}
-
 output "confirmation" {
   value = "Environment B received: ${var.received_value}"
 }
