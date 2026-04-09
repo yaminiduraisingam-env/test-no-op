317606d2d903a6a378276f1a1904a3a870cbe490
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-09 10:31:19 -0400

    Update main.tf

--- env-a/main.tf
+++ env-a/main.tf
@@ -2,6 +2,6 @@ terraform {
   required_version = ">= 1.0"
 }
 
-output "shared_value" {
-  value = "hello-from-env-a-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+output "cluster_config" {
+  value = "cluster-config-ready-${formatdate("YYYYMMDDhhmmss", timestamp())}"
 }
