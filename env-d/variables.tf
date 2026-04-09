457d95605ae0a3f2e8d769f4f045316d96af59fb
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-09 10:33:55 -0400

    Update variables.tf

--- env-d/variables.tf
+++ env-d/variables.tf
@@ -1,1 +1,9 @@
+variable "networking_status" {
+  type        = string
+  description = "Networking status received from env-b"
+}
 
+variable "storage_status" {
+  type        = string
+  description = "Storage status received from env-c"
+}
